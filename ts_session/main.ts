bdfb71021b8e3d86b40e1753120e9db7e0747e18
Nigel Faria <nigel.faria@gmail.com>
2023-02-13 10:12:35 +0000

    d1

---
+++ ts_session/main.ts
@@ -0,0 +1,13 @@
+const aString:string = 'hello';
+console.log(aString);
+// ^? let aString:
+
+
+interface User {
+    id: number;
+}
+const users : User[] = [];
+
+const newUsers = [1,2,3];
+newUsers.push("asdfsa");
+
